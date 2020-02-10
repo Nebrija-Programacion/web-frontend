22f9c028969924467f521b59879a14939f37ec05
avalero <avalero@nebrija.es>
2020-02-10 09:17:51 +0100

    missing files

--- sesion2/ejer2/src/components/Alumnos.jsx
+++ sesion2/ejer2/src/components/Alumnos.jsx
@@ -8,15 +8,13 @@ const Alumnos = props => {
     props.alumnos :
     props.alumnos.filter( obj => {
       if (props.aprobado === 1) return obj.nota >= 5;
-      if (props.aprobado === 2) return obj.nota < 5;
+      else if (props.aprobado === 2) return obj.nota < 5;
     });
 
   return (
     <div className="Alumnos">
       {filtered.map(alumno => (
-        <div className="Alumno"
-          key={alumno.id}
-        >
+        <div className="Alumno" key={alumno.id}>
           <div className="AlumnoData">{alumno.nombre}</div>
           <div className="AlumnoData">{alumno.nota}</div>
         </div>
