93b141b04c766f45677ae6040b2d6c574178b731
avalero <alberto.valero@bq.com>
2020-04-07 12:30:07 +0200

    weather

---
+++ weatherfunctional/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "./components/Header/Header";
+import Body from "./components/Body/Body";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+export default App;
---
+++ weatherfunctional/src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Header.css";
+
+const Header = props => {
+  return <div className="Header">Weather App</div>;
+};
+
+export { Header as default };
