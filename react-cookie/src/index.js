33f3562e718910b8c127cc6cf79049467814af57
avalero <alberto.valero@bq.com>
2020-04-08 17:01:34 +0200

    view menu

--- react-cookie/src/index.js
+++ react-cookie/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { CookiesProvider } from "react-cookie";
 
 ReactDOM.render(
   <React.StrictMode>
