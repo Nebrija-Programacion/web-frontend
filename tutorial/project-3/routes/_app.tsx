fc771ffe7765364874e126312ea1e686b438820a
Alberto Valero <alberto.valero@bq.com>
2024-02-17 11:52:26 +0100

    title

--- tutorial/project-3/routes/_app.tsx
+++ tutorial/project-3/routes/_app.tsx
@@ -5,7 +5,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>fresh-6</title>
+        <title>Fresh project 3</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
