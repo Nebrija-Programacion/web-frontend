bdeac7c7d0842c8bdc44805b9128169893cb2de9
Alberto Valero Gómez <avalero@users.noreply.github.com>
2024-03-08 13:14:04 +0100

    Update Form.tsx

--- tutorial/fresh-10/islands/Form.tsx
+++ tutorial/fresh-10/islands/Form.tsx
@@ -1,45 +1,67 @@
 import { useState } from "preact/hooks";
 import { FunctionComponent } from "preact";
-
-type Error = {
-  error: boolean;
-  message: string;
-};
+import { JSX } from "preact";
 
 export const Form: FunctionComponent = () => {
-  const [error, setError] = useState<Error>({
-    error: false,
-    message: "",
-  });
+  const [error, setError] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [age, setAge] = useState<number | undefined>();
+  const [email, setEmail] = useState<string>("");
 
-  const checkAge = (value: number) => {
-    if (value < 18) {
-      setError({
-        error: true,
-        message: "You must be older than 18",
-      });
-    } else {setError({
-        error: false,
-        message: "",
-      });}
-  };
+  const submitHandler = (e:JSX.TargetedEvent<HTMLFormElement, Event>) => {
+    e.preventDefault();
+    const errorMsg: string[] = [];
+    if (!age || age < 18) {
+      errorMsg.push("Age must be greater than 18");
+    }
+    if (name === "") {
+      errorMsg.push("You must provide a name");
+    }
+    if (email === "") {
+      errorMsg.push("You must provide a mail");
+    }
+
+    if (errorMsg.length > 0) setError(errorMsg.join(" | "));
+    else {
+      setError("");
+      e.currentTarget.submit();
+    }
+  }
+  
 
   return (
     <div class="form">
       <h1>Introduce tus datos</h1>
-      <form action="/submitform.tsx" method="POST">
+      <form
+        action="/submitform.tsx"
+        method="POST"
+        onSubmit={submitHandler}
+          
+      >
         <div>
           <label for="name">Name</label>
         </div>
         <div>
-          <input type="text" id="name" name="name" />
+          <input
+            onFocus={(e) => setError("")}
+            onInput={(e) => setName(e.currentTarget.value)}
+            type="text"
+            id="name"
+            name="name"
+          />
         </div>
 
         <div>
           <label for="email">Email</label>
         </div>
         <div>
-          <input type="email" id="email" name="email" />
+          <input
+            onFocus={(e) => setError("")}
+            onInput={(e) => setEmail(e.currentTarget.value)}
+            type="email"
+            id="email"
+            name="email"
+          />
         </div>
 
         <div>
@@ -47,23 +69,37 @@ export const Form: FunctionComponent = () => {
         </div>
         <div>
           <input
+            onFocus={(e) => setError("")}
             type="number"
             id="age"
             name="age"
-            onBlur={(e) => checkAge(Number(e.currentTarget.value))}
+            onInput={(e) => setAge(Number(e.currentTarget.value))}
           />
         </div>
         <div>
-          <button disabled={error.error} type="submit" class="btn">
+          <button
+            type="submit"
+            disabled={error !== ""}
+            class="btn"
+          >
             Submit
           </button>
         </div>
         <div>
-          <button type="reset" class="reset">
+          <button
+            type="reset"
+            class="reset"
+            onClick={(e) => {
+              setName("");
+              setEmail("");
+              setAge(undefined);
+              setError("");
+            }}
+          >
             Reset
           </button>
         </div>
-        {error.error && <div class="span-2 error">{error.message}</div>}
+        {error !== "" && <div class="span-2 error">{error}</div>}
       </form>
     </div>
   );
