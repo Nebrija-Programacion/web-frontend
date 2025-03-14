4aae60556f065afe42b0c3dc538bdf0f82777a49
Alberto Valero Gómez <avalero@users.noreply.github.com>
2025-03-14 11:44:23 +0000

    wip

---
+++ ejemplos/rickymorty/components/Character.tsx
@@ -0,0 +1,22 @@
+import { FunctionalComponent } from "preact/src/index.d.ts";
+
+type Props = {
+    name: string;
+    status: string;
+    origin: string;
+    image: string;
+}
+
+const Character: FunctionalComponent<Props> = (props) => {
+    return (
+        <div>
+            <img src={props.image} alt={props.name}/>
+            <div>
+                <div>Status: {props.status}</div>
+                <div>Origin: {props.origin}</div>
+            </div>
+        </div>
+    )
+}
+
+export default Character;
---
+++ ejemplos/rickymorty/components/CharacterContainer.tsx
@@ -0,0 +1,27 @@
+import { FunctionalComponent } from "preact/src/index.d.ts";
+import CharacterComponent from "./Character.tsx"
+type Character = {
+  id: string;
+  name: string;
+  status: string;
+  origin: string;
+  image: string;
+  episodes: Array<{ name: string; id: string }>;
+};
+
+type Props = {
+  character: Character;
+};
+
+const CharacterContainer: FunctionalComponent<Props> = (props) => {
+  const ch = props.character;
+  return (
+    <div>
+      <h1>{ch.name}</h1>
+    <CharacterComponent name={ch.name} origin={ch.origin} status={ch.status} image={ch.image}/>
+      <ul>{ch.episodes.map((ep) => <li key={ep.id}>{ep.name}</li>)}</ul>
+    </div>
+  );
+};
+
+export default CharacterContainer;
