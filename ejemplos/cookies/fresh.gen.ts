d53562c430d7c9f2542b98ab0e3033366250540a
Alberto Valero Gómez <avalero@users.noreply.github.com>
2025-05-23 11:19:46 +0000

    full cookies example

--- ejemplos/cookies/fresh.gen.ts
+++ ejemplos/cookies/fresh.gen.ts
@@ -5,6 +5,7 @@
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $api_joke from "./routes/api/joke.ts";
+import * as $cookiessr from "./routes/cookiessr.tsx";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
 import * as $ruta from "./routes/ruta.tsx";
@@ -16,6 +17,7 @@ const manifest = {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
     "./routes/api/joke.ts": $api_joke,
+    "./routes/cookiessr.tsx": $cookiessr,
     "./routes/greet/[name].tsx": $greet_name_,
     "./routes/index.tsx": $index,
     "./routes/ruta.tsx": $ruta,
